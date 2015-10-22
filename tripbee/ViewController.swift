3fc858422d201e209de7f68b4fc87866242a0159
박정환 <tmozii1@gmail.com>
2015-10-22 15:22:36 +0900

    project init

---
+++ tripbee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tripbee
+//
+//  Created by ParkJH on 2015. 10. 22..
+//  Copyright © 2015년 timworld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
